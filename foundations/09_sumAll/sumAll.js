402e9fa5756ba084b8d4c57b4646d7621b41d2e8
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-01-26 17:16:59 +0530

    do exercise 9 of foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    let areIntegers = Number.isInteger(num1) && Number.isInteger(num2);
+    let arePositiveIntegers = (areIntegers) && (num1 > 0) && (num2 > 0);
+
+    if(!arePositiveIntegers)
+        return "ERROR";
+
+    //Start with the smaller of the two up until the bigger of the two
+    let i = Math.min(num1, num2);
+    while(i <= Math.max(num1, num2)) {
+        sum += i;
+        i++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
