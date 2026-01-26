4ae0a71c0dd3e7f220db8ac1f8bee3d7022e206a
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-01-26 17:17:27 +0530

    do exercise 10 of foundations

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if( year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0 ) )
+        return true;
+    return false;
 
+    // let isYearDivisibleByFour = (year % 4 == 0);
+    // let isCentury = (year % 100 == 0);
+    // let isYearDivisibleByFourHundred = (year % 400 == 0);
+
+    // if( isYearDivisibleByFour && ( !isCentury || isYearDivisibleByFourHundred ) )
+    //     return true;
+    // return false;
 };
 
 // Do not edit below this line
