98cb5861b545351f45f076ea3d4652246620827f
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-01-26 17:16:30 +0530

    do exercise 8 of foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    //iterate through elementsToRemove to remove each elements
+    for(let elem of elementsToRemove) {
+        //While array contains elem, Remove it
+        while(array.includes(elem)) {
+            array.splice(
+                array.findIndex( item => (item == elem) ),
+                1
+            );
+        }
+    }
+    
+    return array
+
+
+    // let newArray = [];
+
+    // array.forEach(
+    //     (item) => {
+    //         if(!elementsToRemove.includes(item))
+    //             newArray.push(item);
+    //     }
+    // )
+
+    // return newArray;
 };
 
 // Do not edit below this line
