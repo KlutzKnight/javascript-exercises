998f4dfa31bab5532c8b1a3c58da9664906da199
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-01-26 17:18:00 +0530

    do exercise 11 of foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let multiplicationFactor = 1.8;
+  let additionFactor = 32;
+  let tempInCelsius = (temp - additionFactor) / multiplicationFactor;
+
+  return Math.round( ( tempInCelsius * 10 )) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let multiplicationFactor = 1.8;
+  let additionFactor = 32;
+  let tempInFahrenheit = (multiplicationFactor * temp) + additionFactor;
+
+  return Math.round((tempInFahrenheit * 10)) / 10;
 };
 
 // Do not edit below this line
