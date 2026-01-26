8a3d7e5c5e3ea1bb5879b44aab011eeef88ac6ec
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-01-26 17:14:14 +0530

    do exercise 6 of foundations

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedStrring = "";
+    
+    if(num < 0) {
+        return "ERROR";
+    }
 
+    for(let i = 0; i < num; i++) {
+        repeatedStrring += string;
+    }
+
+    return repeatedStrring;
 };
 
 // Do not edit below this line
