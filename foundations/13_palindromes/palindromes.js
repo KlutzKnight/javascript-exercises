4a51d58ce8297d5e1da0027311e1d2fb0d953e22
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-02-17 21:08:19 +0530

    feat: complete exercise 12 and 13 of foundations

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+function isAlphaNumeric(str) {
+  return str.match(/^[a-zA-Z0-9]+$/i) !== null;
+}
 
+
+const palindromes = function (str) {
+    let reverse = "";
+    for(let i = str.length - 1; i >= 0; i--) {            
+        reverse += str[i];
+    }
+    
+    const strAlNum = str.replace(/[^a-zA-Z0-9]/g, '')
+    const reverseAlNum = reverse.replace(/[^a-zA-Z0-9]/g, '')
+    return reverseAlNum.toLowerCase() == strAlNum.toLowerCase();
 };
 
 // Do not edit below this line
