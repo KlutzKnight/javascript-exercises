cbe1b1019c5b5ac03cd638b9a7eea95107c1d6de
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-02-17 21:52:29 +0530

    complete exercise 14 of Foundations

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0)
+        return "OOPS"
 
+    if(n < 2)
+        return parseInt(n);
+    
+    let firstFib = 0;
+    let secondFib = 1;
+    let nextFib;
+    for(let i = 2; i <= n; i++) {
+        nextFib = firstFib + secondFib;
+        firstFib = secondFib;
+        secondFib = nextFib;
+    }
+
+    return nextFib;
 };
 
 // Do not edit below this line
