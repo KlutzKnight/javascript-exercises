497fccc6ccab65678f613d1782b6cd1d8a8d554d
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-02-17 22:19:16 +0530

    Complete exercise 15 and 16 of Foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = {};
+    let oldestPersonAge = 0;
+    let currentYear = new Date().getFullYear();
 
+    for(let i = 0; i < people.length; i++) {
+        if(people[i].yearOfDeath == undefined)
+            currentPersonAge = currentYear - people[i].yearOfBirth;
+        else
+            currentPersonAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        
+        if(currentPersonAge > oldestPersonAge) {
+            oldestPersonAge = currentPersonAge;
+            oldestPerson = people[i];
+        }
+    }
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
